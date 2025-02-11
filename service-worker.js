851a4dde30943ff1426df8fea9b9440c8bdcfba2
Christina Andersen <64217823+meCodeUp@users.noreply.github.com>
2025-02-11 20:44:19 +0100

    Update service-worker.js
    neuer service worker für offline. Chashed alles

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,21 @@
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "offline-pwa_EggTimer-";
+
+// Import Workbox library
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
-const offlineFallbackPage = "offline.html";
-
-// Skip waiting
+// Skip waiting and activate new Service Worker immediately
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
 
-// Cache the offline fallback page during install
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-// Enable navigation preload
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-// Fetch event handler
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+// Cache all requests with a StaleWhileRevalidate strategy
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
